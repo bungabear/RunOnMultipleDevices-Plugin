8c41fccbf97f073eefc86b7cf1f1ad75ecba47f3
bungabear <bungabear6422@gmail.com>
2020-12-14 18:27:31 +0900

    Support Android Studio 4.1. close #1

--- src/main/kotlin/action/RunOnMultipleDevicesPopupAction.kt
+++ src/main/kotlin/action/RunOnMultipleDevicesPopupAction.kt
@@ -4,16 +4,41 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
+
 class RunOnMultipleDevicesPopupAction : AnAction() {
+    private val methods = arrayOf(this::tryMethod1, this::tryMethod2)
     override fun actionPerformed(e: AnActionEvent) {
-        val deviceAction = ActionManager.getInstance().getAction("DeviceAndSnapshotComboBox")
-        try {
-            val field = deviceAction.javaClass.declaredFields.first { it.name == "myRunOnMultipleDevicesAction" }
-            field.isAccessible = true
-            val targetAction = field.get(deviceAction) as AnAction
-            targetAction.actionPerformed(e)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        methods.first { method ->
+            try{
+                method.invoke(e)
+                true
+            } catch (ex: Exception){
+                ex.printStackTrace()
+                false
+            }
         }
     }
+
+    // Android Studio ~ 4.0
+    private fun tryMethod1(e: AnActionEvent) {
+        val deviceAction = ActionManager.getInstance().getAction("RunOnMultipleDevicesAction")
+        deviceAction.actionPerformed(e)
+    }
+
+    // Android Studio 4.1 ~
+    private fun tryMethod2(e: AnActionEvent) {
+        val deviceAction = ActionManager.getInstance().getAction("DeviceAndSnapshotComboBox")
+        val field = deviceAction.javaClass.declaredFields.first { it.name == "myRunOnMultipleDevicesAction" }
+        field.isAccessible = true
+        val targetAction = field.get(deviceAction) as AnAction
+        targetAction.actionPerformed(e)
+    }
+
+    // show context menu
+    // not use
+    private fun tryMethod3(e: AnActionEvent) {
+        val deviceAction = ActionManager.getInstance().getAction("DeviceAndSnapshotComboBoxAction")
+        deviceAction.actionPerformed(e)
+    }
+
 }
